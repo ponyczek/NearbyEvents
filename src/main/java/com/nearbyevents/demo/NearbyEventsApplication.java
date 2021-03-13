895b0536f37428bec94196898f0416fe689618c9
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2021-03-13 20:20:49 +0100

    initial commit

---
+++ src/main/java/com/nearbyevents/demo/NearbyEventsApplication.java
@@ -0,0 +1,13 @@
+package com.nearbyevents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearbyEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nearbyevents/demo/NearbyEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearbyevents.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
